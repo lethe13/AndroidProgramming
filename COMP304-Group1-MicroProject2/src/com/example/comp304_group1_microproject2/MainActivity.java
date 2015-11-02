d509ee8c5fb53d9437f0b794cf58374ccb9df239
drkdagron <ste.mckillop@gmail.com>
2015-11-02 14:54:28 -0500

    Added Micro Project 2

---
+++ COMP304-Group1-MicroProject2/src/com/example/comp304_group1_microproject2/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.comp304_group1_microproject2;
+
+import android.app.Activity;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+	MediaPlayer mp;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		mp = MediaPlayer.create(this, R.raw.frozen_letitgo);
+		mp.start();
+		
+		TextView tv = (TextView)findViewById(R.id.SongList);
+		tv.setText("Song Length: " + mp.getDuration() + "ms");
+	}
+	
+	@Override
+	protected void onPause() { super.onPause(); mp.stop(); }
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
