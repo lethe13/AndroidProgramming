be2aa6383f3acbeaa9676c6d8d56192faf63869e
Robert Riemens <rriemens@my.centennialcollege.ca>
2015-12-13 12:55:43 -0500

    ohhhboy
    my contribution

--- COMP304-Group1-MicroProject6/gen/com/example/comp304_group1_microproject5/R.java
+++ COMP304-Group1-MicroProject6/gen/com/example/comp304_group1_microproject5/R.java
@@ -22,33 +22,35 @@ public final class R {
         public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int casinogames=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int Score1=0x7f090014;
-        public static final int Score2=0x7f090015;
-        public static final int action_settings=0x7f090017;
+        public static final int Score1=0x7f090015;
+        public static final int Score2=0x7f090016;
+        public static final int action_settings=0x7f090018;
         public static final int b_login=0x7f090008;
         public static final int button1=0x7f09000a;
-        public static final int button2=0x7f090009;
-        public static final int button3=0x7f09000b;
-        public static final int button4=0x7f09000c;
+        public static final int button2=0x7f09000b;
+        public static final int button3=0x7f09000c;
+        public static final int button4=0x7f09000d;
         public static final int con_pass=0x7f090010;
-        public static final int drawarea=0x7f090005;
-        public static final int lblChance=0x7f090002;
-        public static final int lblScore=0x7f090003;
-        public static final int login=0x7f090000;
-        public static final int reg=0x7f090001;
+        public static final int drawarea=0x7f090006;
+        public static final int imageView1=0x7f090000;
+        public static final int lblChance=0x7f090003;
+        public static final int lblScore=0x7f090004;
+        public static final int login=0x7f090001;
+        public static final int reg=0x7f090002;
         public static final int reg_pass=0x7f09000f;
         public static final int reg_user=0x7f09000e;
-        public static final int tblGame=0x7f090004;
-        public static final int textView1=0x7f090011;
-        public static final int textView2=0x7f090012;
-        public static final int textView3=0x7f090013;
-        public static final int totalScore=0x7f090016;
-        public static final int user_name=0x7f090007;
-        public static final int user_pass=0x7f090006;
-        public static final int user_reg=0x7f09000d;
+        public static final int tblGame=0x7f090005;
+        public static final int textView1=0x7f090012;
+        public static final int textView2=0x7f090013;
+        public static final int textView3=0x7f090014;
+        public static final int totalScore=0x7f090017;
+        public static final int user_name=0x7f090009;
+        public static final int user_pass=0x7f090007;
+        public static final int user_reg=0x7f090011;
     }
     public static final class layout {
         public static final int activity_first_page=0x7f030000;
@@ -74,6 +76,9 @@ public final class R {
         public static final int action_settings=0x7f060002;
         public static final int app_name=0x7f060000;
         public static final int hello_world=0x7f060001;
+        public static final int login=0x7f060009;
+        public static final int logo=0x7f060007;
+        public static final int register=0x7f060008;
         public static final int title_activity_first_page=0x7f060004;
         public static final int title_activity_graphical_guessing_game=0x7f060003;
         public static final int title_activity_login=0x7f060006;
